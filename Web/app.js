2675cba2371fae3f4cf0a1c8f6273f8fb335c20b
Joffrey NURIT <4489033+joffreynurit@users.noreply.github.com>
2022-09-09 23:59:16 +0200

    https

--- Web/app.js
+++ Web/app.js
@@ -1,5 +1,5 @@
 jQuery.ajax({
-    url: "http://communities-events.azurewebsites.net/api/GetNextMeetupEvents?code=q4nYZ5Ehr84QXgzo3ypZO8c4c8rwZ2obLjvP4tCWqNyuAzFuVCACYA==",
+    url: "https://communities-events.azurewebsites.net/api/GetNextMeetupEvents?code=q4nYZ5Ehr84QXgzo3ypZO8c4c8rwZ2obLjvP4tCWqNyuAzFuVCACYA==",
     success: function( result ) {
         jQuery( "#meetup-events" ).html( result );
     }
